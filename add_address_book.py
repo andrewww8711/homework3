8667a28a83c0493df1ca94223f0a258becd5810a
Andrey <avizovskiy@yahoo.com>
2018-04-14 16:50:05 -0700

    Added group

--- add_address_book.py
+++ add_address_book.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from group import Group
 import unittest
 
 def is_alert_present(wd):
@@ -19,39 +20,48 @@ class add_address_book(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_address_book_page(wd)
-        self.create_address_book(wd, firstname="Ivan", lastname="Ivanov", title="QA Engineer", company="Google", address="123 main street", homephone="123456", cellphone="1234567",
-                                 email="test@mail.com")
+        self.create_address_book(wd, Group(firstname="Ivan", lastname="Ivanov", title="QA Engineer", company="Google", address="123 main street", homephone="123456", cellphone="1234567",
+                                 email="test@mail.com"))
+        self.logout(wd)
+
+    def test_empty_add_address_book(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_address_book_page(wd)
+        self.create_address_book(wd, Group(firstname="", lastname="", title="", company="", address="", homephone="", cellphone="",
+                                 email=""))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_address_book(self, wd, firstname, lastname, title, company, address, homephone, cellphone, email):
+    def create_address_book(self, wd, group):
         # fill in the form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(group.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(group.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(group.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(group.homephone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(cellphone)
+        wd.find_element_by_name("mobile").send_keys(group.cellphone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(group.email)
         # submit the form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
---
+++ group.py
@@ -0,0 +1,10 @@
+class Group:
+    def __init__(self, firstname, lastname, title, company, address, homephone, cellphone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone = homephone
+        self.cellphone = cellphone
+        self.email = email
